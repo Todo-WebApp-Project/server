f5067b6359eff6c8899ab6d52fdea4caabad02e3
YoonYn9915 <jsy440720@gmail.com>
2024-03-29 00:23:07 +0900

    KAN-14 GoogleCalendarController.calendar 토큰 설정, EventListService에서 Json 데이터 Scheduel로 변환 후 DB에 저장

--- src/main/java/com/example/demo/controller/GoogleCalendarController.java
+++ src/main/java/com/example/demo/controller/GoogleCalendarController.java
@@ -1,12 +1,12 @@
 package com.example.demo.controller;
 
+import com.example.demo.Jwt.JwtFunc;
 import com.example.demo.service.CalendarListService;
 import com.example.demo.service.EventListService;
 import com.example.demo.service.RestJsonService;
 import org.json.JSONException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.json.JSONObject;
 
@@ -24,6 +24,7 @@ public class GoogleCalendarController {
     EventListService eventListService;
     String accessToken;     //구글 OAuth 2.0 인증 후 받은 사용자의 accessToken
 
+    String user_id;
 
 
     @Autowired
@@ -31,20 +32,23 @@ public class GoogleCalendarController {
         super();
         this.calendarListService = calendarListService;
         this.eventListService = eventListService;
-
     }
 
 
     @GetMapping("/calendar")
-    public String calendar(Model model) {
+    public String calendar(@RequestHeader("Authorization") String token)
+    {
+        JwtFunc jwt = new JwtFunc();
+        user_id = jwt.unpackJWT(token);
+
         return "calendar";
     }
 
 
+
     @GetMapping("/test")
-    public String test(@RequestParam("code") String code, Model model) 
+    public String test(@RequestParam("code") String code)
     {
-        String eventJsonData = null;
         RestJsonService restJsonService = new RestJsonService();
         String accessTokenJsonData = restJsonService.getAccessTokenJsonData(code);
 
@@ -64,10 +68,7 @@ public class GoogleCalendarController {
         //calendarList에서 캘린더의 id하나씩 가져와서 캘린더 조회.
         for (String id : calendarIdList) {
             try {
-                /*
-                 아직 못한것 : EventListService.java에서 Json 문자열을 클래스에 매핑해야 됌.
-                 */
-                eventJsonData = eventListService.getMyCalendarEventList(accessToken, URLEncoder.encode(id, StandardCharsets.UTF_8.toString()));
+                eventListService.getMyCalendarEventList(user_id,accessToken, URLEncoder.encode(id, StandardCharsets.UTF_8.toString()));
 
             } catch (UnsupportedEncodingException e) {
                 e.printStackTrace();
@@ -76,9 +77,5 @@ public class GoogleCalendarController {
 
         return "success";
     }
-
-
-
-
 
 }
--- src/main/java/com/example/demo/service/EventListService.java
+++ src/main/java/com/example/demo/service/EventListService.java
@@ -3,24 +3,41 @@ package com.example.demo.service;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.URL;
-import java.time.LocalDateTime;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
+import com.example.demo.domain.User;
+import com.example.demo.exception.UserNotFoundException;
+import com.example.demo.repository.ScheduleRepository;
+import com.example.demo.repository.UserRepository;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.stereotype.Service;
+import com.example.demo.domain.Schedule;
 
 @Service
 public class EventListService {
     private final String HTTP_REQUEST_PRE = "https://www.googleapis.com/calendar/v3/calendars/";
     private final String HTTP_REQUEST_POST = "/events";
 
-    public String getMyCalendarEventList(String accessToken, String encodedCalendarId){
+    private UserRepository userRepository;
+    private ScheduleRepository scheduleRepository;
+
+
+    public EventListService(UserRepository userRepository, ScheduleRepository scheduleRepository){
+        this.userRepository = userRepository;
+        this.scheduleRepository = scheduleRepository;
+    }
+    public void getMyCalendarEventList(String user_id,String accessToken, String encodedCalendarId){
         try {
             String jsonData = "";
-
-            LocalDateTime dateTime = LocalDateTime.now().minusYears(3);
+            List<Schedule> scheduleList = new ArrayList<>();
 
             // Constructing the URL for the Google Calendar API request
             URL url = new URL(HTTP_REQUEST_PRE + encodedCalendarId + HTTP_REQUEST_POST +
-                    "?access_token=" + accessToken + "&timeMax="+dateTime+"Z"
+                    "?access_token=" + accessToken
             );
 
             BufferedReader bf;
@@ -30,10 +47,52 @@ public class EventListService {
                 jsonData+=line;
             }
 
-            return jsonData;
+            JsonNode rootNode =  new ObjectMapper().readTree(jsonData);
+            JsonNode itemsNode = rootNode.get("items");
 
-        } catch(Exception e) {
-            return "error";
+            Optional<User> optionalUserFindByUserId = userRepository.findById(user_id);
+
+
+            for (JsonNode item : itemsNode) {
+                String creatorEmail = item.get("creator").get("email").asText();
+
+                if(optionalUserFindByUserId.isPresent()){
+                    if(!creatorEmail.equalsIgnoreCase(optionalUserFindByUserId.get().getEmail()))
+                        continue;
+                }else
+                {
+                    return;
+                }
+
+                String startDateTime = item.get("start").get("dateTime").asText();
+                startDateTime = startDateTime.substring(0,startDateTime.indexOf('T'));
+                String endDateTime = item.get("end").get("dateTime").asText();
+                endDateTime = endDateTime.substring(0,endDateTime.indexOf('T'));
+                String summary = item.get("summary").asText();
+
+                Optional<User> optionalUserFindByEmail = userRepository.findByEmail(creatorEmail);
+                if (optionalUserFindByEmail.isPresent()) {
+                    User user = optionalUserFindByEmail.get();
+
+                    Schedule schedule = new Schedule();
+                    schedule.setUser(user);
+                    schedule.setStart_date(LocalDate.parse(startDateTime));
+                    System.out.println(LocalDate.parse(startDateTime));
+                    schedule.setEnd_date(LocalDate.parse(endDateTime));
+                    schedule.setPlan(summary);
+                    schedule.setId(30010);
+                    schedule.setColor("dcdcdc");
+
+
+                    scheduleRepository.save(schedule);
+                } else {
+                    throw new UserNotFoundException("User with email " + creatorEmail + " not found.");
+                }
+            }
+
+        }catch(Exception e)
+        {
+            e.printStackTrace();
         }
     }
 }
